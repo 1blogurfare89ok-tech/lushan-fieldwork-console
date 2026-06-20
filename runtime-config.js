fb22853e9dc48960301ed75a05cb6565e623f6ce
1blogurfare89ok-tech <1blogurfare89ok@gmail.com>
2026-06-20 10:20:19 +0800

    Initial deploy of Lushan Fieldwork Console

--- runtime-config.js
+++ runtime-config.js
@@ -1,4 +1,8 @@
-window.LUSHAN_CONFIG = Object.freeze({ apiBase: "http://localhost:8000" });
+window.LUSHAN_CONFIG = Object.freeze({
+  apiBase: "http://localhost:8000",
+  amapKey: "46eae30183860ed3629380d231d1bb4",
+  amapSecurityCode: "ac7d238b2a0838e7645c631a7cc49dd"
+});
 document.addEventListener("DOMContentLoaded", () => {
   const input = document.querySelector("#apiBase");
   const saved = localStorage.getItem("lushanApiBase");
